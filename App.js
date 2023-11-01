21a6ec6949dfd3cd4ebff3bac379d8eeae72d22a
Raiyan Arsh <1165raiyan@gmail.com>
2023-11-01 21:35:04 +0530

    abc

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import BottomNavigation from './components/navigation/bottom.js'; // Import the BottomNavigation component
+import Find from './components/scre/find.js'
+import Profile from './components/scre/profile.js'
+import Records from './components/scre/record.js'
+import Application from './components/scre/application.js'
+import Login from './components/login.js'; // Import the Login component
 
 export default function App() {
+  const [selectedTab, setSelectedTab] = useState('Home');
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  // Conditionally render the login screen if the user is not logged in
+  if (!userLoggedIn) {
+    return (
+      <Login onLogin={() => setUserLoggedIn(true)} />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {selectedTab === 'Home' && <Find />}
+      {selectedTab === 'Profile' && <Profile />}
+      {selectedTab === 'Records' && <Records />}
+      {selectedTab === 'Application' && <Application />}
+
+      {/* Include the bottom navigation bar */}
+      <BottomNavigation selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
     </View>
   );
 }
@@ -14,7 +35,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  pageContainer: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
